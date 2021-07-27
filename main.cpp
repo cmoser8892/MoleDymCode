bbe663af7404e553f71331b8adecb91b6c183257
Christoph Moser <christoph.mos.studium@gmail.com>
2021-07-27 21:24:54 +0200

    - Report - i want to remake the graphs from milestone 4 they are way to inconsistent and do not fit the current style

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
     std::cout << "Molecular Dynamics Project" << std::endl;
-    //return milestone4Code();
+    return milestone4Code();
     //return milestone5Code(argc,argv);
     //return milestone6Code(argc,argv);
-    return milestone7Code(argc,argv);
+    //return milestone7Code(argc,argv);
     //return generateClusterHull(argc,argv);
     //return writeClusters();
 }
